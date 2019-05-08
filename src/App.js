28b5587aa32d4e649cf847996b79372328f548b5
mintchkin <mintchkin.dobfner@gmail.com>
2019-05-08 01:47:09 -0400

    Add keyboard handler

--- src/App.js
+++ src/App.js
@@ -4,11 +4,46 @@ import ChoiceList from "ChoiceList";
 import RestartButton from "RestartButton";
 
 const sampleChoices = [["U", "U"], ["U", "D"], ["D", "U"], ["D", "D"]];
+const choices = Object.freeze({
+  LEFT: "L",
+  RIGHT: "R"
+});
 
 class App extends Component {
   state = {
     choices: sampleChoices
   };
+
+  handleKeydown = event => {
+    switch (event.key) {
+      case "Left":
+      case "ArrowLeft":
+        this.addChoice(choices.LEFT);
+        break;
+      case "Right":
+      case "ArrowRight":
+        this.addChoice(choices.RIGHT);
+        break;
+      default:
+        return;
+    }
+
+    event.preventDefault();
+  };
+
+  addChoice(choice) {
+    this.setState(state => ({
+      choices: [...state.choices, [choice, choices.LEFT]]
+    }));
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeydown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeydown);
+  }
 
   render() {
     return (
